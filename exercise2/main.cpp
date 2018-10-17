8fdc8da52332691ede052db9309594da41082e6c
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2018-10-17 19:29:23 +0200

    lets make this s**t in oop

--- exercise2/main.cpp
+++ exercise2/main.cpp
@@ -9,91 +9,124 @@ using namespace std;
 #define SALES 4
 #define PRODUCTS 5
 
-void assert_in_range(int64_t num, int64_t begin, int64_t end);
-float &get_sales_product(float *data, int64_t sales, int64_t product);
-void read_tsv_line(float *data);
-void abort(float *data, const char *reason);
-void print(float *data);
-
-int main()
+class Product
 {
-  float *data = new float[SALES * PRODUCTS];
-  memset(data, 0, sizeof(float) * SALES * PRODUCTS);
+private:
+  int64_t val;
 
-  while (true)
+public:
+  Product()
   {
-    read_tsv_line(data);
+    this->val = 0;
   }
 
-  free(data);
-  return 0;
-}
-
-void assert_in_range(float *data, int64_t num, int64_t begin, int64_t end)
-{
-  if (num < begin || num > end)
+  void sell(int64_t value)
   {
-    stringstream ss;
-    ss << "Number " << num << " is outside of range " << begin << ".."
-       << "end!";
-
-    abort(data, ss.str().c_str());
-  }
-}
-
-void abort(float *data, const char *reason)
-{
-  if (*reason)
-  {
-    cerr << reason << endl;
+    this->val += value;
   }
 
-  print(data);
-  exit(0);
-}
-
-float &get_sales_product(float *data, int64_t sales, int64_t product)
-{
-  return data[((sales - 1) * PRODUCTS) + (product - 1)];
-}
-
-void read_tsv_line(float *data)
-{
-  int64_t sales;
-  int64_t product;
-  float value;
-
-  cout << "Enter data: [sales] [product] [value]: ";
-  cin >> sales;
-  assert_in_range(data, sales, 1, SALES);
-  cin >> product;
-  assert_in_range(data, product, 1, PRODUCTS);
-  cin >> value;
-
-  get_sales_product(data, sales, product) += value;
-}
-
-void print(float *data)
-{
-  cout << endl
-       << "Sales" << '\t';
-
-  for (uint64_t product_no = 1; product_no <= PRODUCTS; ++product_no)
+  int64_t value()
   {
-    cout << product_no << '\t';
+    return this->val;
+  }
+};
+
+class Salesman
+{
+private:
+  Product products[PRODUCTS];
+
+public:
+  Product &product(uint64_t product_id)
+  {
+    return this->products[product_id - 1];
+  }
+};
+
+class Shop
+{
+private:
+  Salesman salesmen[SALES];
+
+public:
+  Salesman &salesman(uint64_t sales_id)
+  {
+    return this->salesmen[sales_id - 1];
   }
 
-  cout << endl;
-
-  for (uint64_t sales = 1; sales <= SALES; ++sales)
+  void abort(const char *reason)
   {
-    cout << sales << '\t';
-
-    for (uint64_t product = 1; product <= PRODUCTS; ++product)
+    if (*reason)
     {
-      cout << get_sales_product(data, sales, product) << '\t';
+      cerr << reason << endl;
+    }
+
+    print();
+    exit(0);
+  }
+
+  void print()
+  {
+    cout << endl
+         << "Sales" << '\t';
+
+    for (uint64_t product_no = 1; product_no <= PRODUCTS; ++product_no)
+    {
+      cout << product_no << '\t';
     }
 
     cout << endl;
+
+    for (uint64_t sales_id = 1; sales_id <= SALES; ++sales_id)
+    {
+      cout << sales_id << '\t';
+
+      for (uint64_t product_id = 1; product_id <= PRODUCTS; ++product_id)
+      {
+        cout << (double)this->salesman(sales_id).product(product_id).value() / 100.0 << '\t';
+      }
+
+      cout << endl;
+    }
   }
+
+  void read_tsv_line()
+  {
+    int64_t sales;
+    int64_t product;
+    int64_t value;
+
+    cout << "Enter data: [sales] [product] [value]: ";
+    cin >> sales;
+    assert_in_range(sales, 1, SALES);
+    cin >> product;
+    assert_in_range(product, 1, PRODUCTS);
+    cin >> value;
+
+    this->salesman(sales).product(product).sell(value);
+  }
+
+  void assert_in_range(int64_t num, int64_t begin, int64_t end)
+  {
+    if (num < begin || num > end)
+    {
+      stringstream ss;
+      ss << "Number " << num << " is outside of range " << begin << ".."
+         << end << '!';
+
+      abort(ss.str().c_str());
+    }
+  }
+};
+
+int main()
+{
+  Shop shop;
+
+  while (true)
+  {
+    shop.read_tsv_line();
+  }
+
+  return 0;
 }
